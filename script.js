8082773ea925ee7dbea62101b83aeff5ac80073f
Krawfox <np.arkhanal@gmail.com>
2026-07-13 18:16:31 +0545

    Flip animation works

--- script.js
+++ script.js
@@ -5,13 +5,22 @@ const safeIndex = Math.floor(Math.random() * cards.length);
 console.log(safeIndex);
 
 cards.forEach((card, index) => {
+
     card.addEventListener("click", () => {
 
-        if (index === safeIndex) {
-            alert("You found the safe card!");
-        } else {
-            window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-        }
+        // Flip the clicked card
+        card.classList.add("flipped");
+
+        setTimeout(() => {
+
+            if (index === safeIndex) {
+                alert("You found the safe card!");
+            } else {
+                window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+            }
+
+        }, 2600);
 
     });
+
 });
