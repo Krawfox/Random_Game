fbd5dc8127ec1f4ca0a4565aec4b16114f4757e6
Krawfox <np.arkhanal@gmail.com>
2026-07-14 06:28:54 +0545

    Won img creation and blocked the multi clicked issue

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 const cards = document.querySelectorAll(".card");
-
 const safeIndex = Math.floor(Math.random() * cards.length);
 
-console.log(safeIndex);
+console.log("Safe card:", safeIndex);
+
+let gameOver = false;
+
+cards.forEach((card, index) => {
+    const back = card.querySelector(".back");
+
+    if (index === safeIndex) {
+        back.style.backgroundImage = "url('assets/won.png')";
+    } else {
+        back.style.backgroundImage = "url('assets/card.png')";
+    }
+});
+
 
 cards.forEach((card, index) => {
 
     card.addEventListener("click", () => {
 
-        // Flip the clicked card
+        if (gameOver) return;
+        gameOver = true;
+
+        cards.forEach(c => {
+            c.style.pointerEvents = "none";
+        });
+
         card.classList.add("flipped");
 
         setTimeout(() => {
 
             if (index === safeIndex) {
-                alert("You found the safe card!");
+
+                alert("🎉 You found the safe card!");
+
+
             } else {
+
                 window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
             }
 
         }, 2600);
